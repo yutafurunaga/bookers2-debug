95b5180e8932c85a71342e014a90358d83392f05
yutafurunaga <126681269+yutafurunaga@users.noreply.github.com>
2023-04-02 08:49:50 +0000

    finish

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root to: "homes#top"
+  get "home/about"=> 'homes#about'
+  resources :books, only: [:new, :show, :index, :create, :edit, :update, :destroy]
+  resources :users, only: [:show, :index, :edit, :update]
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+
