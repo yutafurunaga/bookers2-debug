67b110aad770bf1da42427ef337e00a38ed50b04
yutafurunaga <126681269+yutafurunaga@users.noreply.github.com>
2023-04-06 08:30:50 +0000

    非同期通信追加

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,18 +1,22 @@
 class BookCommentsController < ApplicationController
-  
+
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
-  end
-  
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    if @comment.save
+      render :comments
+    else
+      render 'books/show'
+    end
+   end
+
   def destroy
-    BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    book = Book.find(params[:book_id])
+    render :comments
   end
-  
+
   private
 
   def book_comment_params
